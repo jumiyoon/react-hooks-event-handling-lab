4f9e41146991053ab40c251b77748dd987c2bd10
jumst.code@gmail.com <jumiyoon@Jumis-MacBook-Pro.local>
2022-12-29 16:54:32 -0800

    commit tree ce380436cbc66eb9ad0d247a88afc6910bba89e3

--- src/components/Keypad.js
+++ src/components/Keypad.js
@@ -1,8 +1,14 @@
 // Code Keypad Component Here
+import React from "react"
 
 function Keypad (){
+    function enteringPw(){
+        console.log("Entering password...")
+    }
     return (
-        <div></div>
+        <div>
+            <input onChange={enteringPw} type="password" />
+        </div>
     )
 }
 
